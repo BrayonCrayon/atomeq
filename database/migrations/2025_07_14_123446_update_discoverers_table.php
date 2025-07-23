a6b2ca2f2b7ea92ee7155833acd82a4a39f07eb5
Brady Deroy <b.deroy@vehikl.com>
2025-07-23 16:57:11 -0400

    start reworking the update discoverers table migration to account for existing discoverers before linking them with the elements -WIP
    Co-authored-by: Ekaterina Stroganova <e.stroganova@vehikl.com>

--- database/migrations/2025_07_14_123446_update_discoverers_table.php
+++ database/migrations/2025_07_14_123446_update_discoverers_table.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Models\Discoverer;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
@@ -14,38 +16,76 @@ return new class extends Migration
         // Nihonium = RIKEN
         // Flerovium, Moscovium, Livermorium, Oganesson = JINR and LLNL
         // Tennessine = Oak Ridge National Laboratory and Vanderbilt University and
-
-    // TODO:
-    // create a DiscovererType table
-        // a person, institute
-        // add a column to Discoverer's table for type
-
-    /**
-     * Run the migrations.
-     */
+    const targets = [
+            [
+                'name' => 'Geselleschaft für Schwerionenforschung (GSI)',
+                'id' => null,
+                'elements' => ['Darmstadtium', 'Roentgenium', 'Copernicium']
+            ],
+            [
+                'name' => 'RIKEN',
+                'id' => null,
+                'elements' => ['Nihonium']
+            ],
+            [
+                'name' => 'Joint Institute for Nuclear Research',
+                'id' => null,
+                'elements' => ['Flerovium', 'Moscovium', 'Livermorium', 'Oganesson']
+            ],
+            [
+                'name' => 'Lawrence Livermore National Laboratory',
+                'id' => null,
+                'elements' => ['Flerovium', 'Moscovium', 'Livermorium', 'Oganesson']
+            ],
+            [
+                'name' => 'Oak Ridge National Laboratory',
+                'id' => null,
+                'elements' => ['Tennessine']
+            ],
+            [
+                'name' => 'Vanderbilt University',
+                'id' => null,
+                'elements' => ['Tennessine']
+            ],
+        ];
     public function up(): void
     {
-        DB::table('discoverers')
-            ->insert([
-                ['name' => 'GSI Helmholtz Centre for Heavy Ion Research', 'created_at' => now(), 'updated_at' => now()],
-                ['name' => 'RIKEN', 'created_at' => now(), 'updated_at' => now()],
-                ['name' => 'JINR', 'created_at' => now(), 'updated_at' => now()],
-                ['name' => 'LLNL', 'created_at' => now(), 'updated_at' => now()],
-                ['name' => 'Oak Ridge National Laboratory', 'created_at' => now(), 'updated_at' => now()],
-                ['name' => 'Vanderbilt University', 'created_at' => now(), 'updated_at' => now()],
-            ]);
-    }
+        // create an array of all the discoverers that need to get into the database
+        DB::beginTransaction();
+        $discoverersToAdd = collect(self::targets)->map(function (array $discoverer) {
+            $discovererFound = DB::table('discoverers')->where('name', $discoverer['name'])->first();
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        DB::table('discoverers')->whereIn('name', [
-            'GSI Helmholtz Centre for Heavy Ion Research',
-            'RIKEN',
-            'JINR and LLNL',
-            'Oak Ridge National Laboratory and Vanderbilt University and JINR',
-        ])->delete();
+            if ($discovererFound) {
+                $discoverer['id'] = $discovererFound->id;
+            } else {
+                $id = DB::table('discoverers')->insertGetId([
+                    'name' => $discoverer['name'],
+                    'created_at' => Carbon::now(),
+                    'updated_at' => Carbon::now()
+                ]);
+                $discoverer['id'] = $id;
+            }
+
+            return $discoverer;
+        });
+
+        // Load all elements
+        $elements = DB::table('elements')->select('id', 'name')->get();
+
+        // For each entry in $discoverersToAdd map a data structure that will be inserted into the pivot table
+        $dataToInsert = $discoverersToAdd->map(function (array $item) use ($elements) {
+           $relatedElementIds = $elements->whereIn('name', $item['elements'])->pluck('id');
+
+           return $relatedElementIds->map(function (int $elementId) use ($item) {
+               return [
+                   'element_id' => $elementId,
+                   'discoverer_id' => $item['id'],
+                   'created_at' => Carbon::now(),
+                   'updated_at' => Carbon::now()
+               ];
+           });
+        });
+        DB::rollBack();
+        dd($dataToInsert->flatten()->toArray()); // TODO: too much flatenning
     }
 };
