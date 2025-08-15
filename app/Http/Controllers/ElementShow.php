7b95634bff02778f0a9fd0d5fea561a4a8422e91
e.stroganova <e.stroganova@vehikl.com>
2025-08-15 09:02:27 -0400

    Return a single element based on id provided in query params

--- app/Http/Controllers/ElementShow.php
+++ app/Http/Controllers/ElementShow.php
@@ -3,13 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ElementShowRequest;
+use App\Http\Resources\ElementResource;
+use App\Models\Element;
 
 class ElementShow extends Controller
 {
     public function __invoke(ElementShowRequest $request)
     {
-        $params = $request->input('id', []);
+        $element = new ElementResource(Element::findOrFail($request->input('id')));
 
-        return [];
+        //TODO: handle if element is not found - findOrFail() returns 404, create a better error response and bubble it back up
+
+        return $element;
     }
 }
