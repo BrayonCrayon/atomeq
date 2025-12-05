51d1cd3d2cbd4ccab081bb89b2c7c18e7e6287a7
e.stroganova <e.stroganova@vehikl.com>
2025-12-05 17:01:48 -0500

    WIP - Continue working on types table update with relationships
    Co-authored-by: Brady Deroy <b.deroy@vehikl.com>
    Co-authored-by: Joshua Beckers <j.beckers@vehikl.com>

--- database/migrations/2025_12_02_211934_add_relations_to_types_table.php
+++ database/migrations/2025_12_02_211934_add_relations_to_types_table.php
@@ -19,27 +19,37 @@ return new class () extends Migration {
             $nonMetals = $types->whereIn('name', ['noble-gas', 'halogen']);
             $metals = $children->whereNotIn('name', $nonMetals);
 
-            $nonMetals->each(function ($nonmetal) use ($parents) {
+//            TODO: standardize this: either each or map
+            $nonMetals = $nonMetals->each(function ($nonmetal) use ($parents) {
                 $nonmetalParent = $parents->where('name', '=', 'nonmetal');
                 $nonmetal->parent_id = $nonmetalParent[0]->id;
+            })->map(fn($item) => (array)$item);
+
+            $metals = $metals->map(function ($metal) use ($parents) {
+                $metalParent = $parents->where('name', '=', 'metal')->values();
+                $metal->parent_id = $metalParent[0]->id;
+
+                return (array) $metal;
             });
 
-            // TODO: this is indexed weirdly
-            // TODO: fix indexing
-            $metals->each(function ($metal) use ($parents) {
-                $metalParent = $parents->where('name', '=', 'metal');
-                dd($metalParent[6]);
-                $metal->parent_id = $metalParent->id;
-            });
 
-            // TODO: commit updated types to the database
+//            TODO: Fix this
+//            TODO: potentially metal is not filtered out properly, and is updating everything to parent id 7
+//            DB::table('types')->whereIn('id', $nonMetals->pluck("id") )->update([
+//                "parent_id" => $nonMetals[0]->parent_id
+//            ]);
+//            dd($nonMetals->toArray());
+            DB::table('types')->upsert($nonMetals->toArray(), ['id'], ['parent_id']);
+            DB::table('types')->upsert($metals->toArray(), ['id', 'parent_id'], ['parent_id']);
+            //throw new Error("Force rollback");
         });
     }
 
     public function down(): void
     {
         Schema::table('types', function (Blueprint $table) {
-//            $table->dropForeign('parent_id_types_index'); // TODO: Double check the naming of this when the column is created.
+            $table->dropColumn('parent_id');
+            //$table->dropForeign('types_parent_id_foreign');
         });
     }
 };
