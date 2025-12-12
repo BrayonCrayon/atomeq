656bad5d5db77e3d88c550c38defea92e219cbdc
e.stroganova <e.stroganova@vehikl.com>
2025-12-12 08:38:00 -0500

    Update `types` table with parental relationships to other types

--- database/migrations/2025_12_02_211934_add_relations_to_types_table.php
+++ database/migrations/2025_12_02_211934_add_relations_to_types_table.php
@@ -8,48 +8,47 @@ use Illuminate\Support\Facades\Schema;
 return new class () extends Migration {
     public function up(): void
     {
-        DB::transaction(function () {
-            Schema::table('types', function (Blueprint $table) {
-                $table->foreignId('parent_id')->nullable()->default(null)->references('id')->on('types');
+        try {
+            DB::transaction(function () {
+                Schema::table('types', function (Blueprint $table) {
+                    $table->foreignId('parent_id')->nullable()->default(null)->references('id')->on('types');
+                });
+
+                $types = DB::table('types')->get();
+                $parents = $types->whereIn('name', ['metal', 'nonmetal', 'metalloid']);
+                $children = $types->whereNotIn('name', $parents);
+                $nonMetals = $types->whereIn('name', ['noble-gas', 'halogen']);
+                $metals = $children->whereNotIn('name', ['metal', 'nonmetal', 'metalloid', 'noble-gas', 'halogen']);
+
+                $nonMetals = $nonMetals->map(function ($nonMetal) use ($parents) {
+                    $nonMetalParent = $parents->where('name', '=', 'nonmetal')->values();
+                    $nonMetal->parent_id = $nonMetalParent[0]->id;
+
+                    return (array)$nonMetal;
+                });
+
+                $metals = $metals->map(function ($metal) use ($parents) {
+                    $metalParent = $parents->where('name', '=', 'metal')->values();
+                    $metal->parent_id = $metalParent[0]->id;
+
+                    return (array)$metal;
+                });
+
+                DB::table('types')->upsert($nonMetals->toArray(), ['id'], ['parent_id']);
+                DB::table('types')->upsert($metals->toArray(), ['id'], ['parent_id']);
+
             });
+        } catch (Exception $exception) {
+            throw new Error("Force rollback with $exception");
 
-            $types = DB::table('types')->get();
-            $parents = $types->whereIn('name', ['metal', 'nonmetal', 'metalloid']);
-            $children = $types->whereNotIn('name', $parents);
-            $nonMetals = $types->whereIn('name', ['noble-gas', 'halogen']);
-            $metals = $children->whereNotIn('name', $nonMetals);
-
-//            TODO: standardize this: either each or map
-            $nonMetals = $nonMetals->each(function ($nonmetal) use ($parents) {
-                $nonmetalParent = $parents->where('name', '=', 'nonmetal');
-                $nonmetal->parent_id = $nonmetalParent[0]->id;
-            })->map(fn($item) => (array)$item);
-
-            $metals = $metals->map(function ($metal) use ($parents) {
-                $metalParent = $parents->where('name', '=', 'metal')->values();
-                $metal->parent_id = $metalParent[0]->id;
-
-                return (array) $metal;
-            });
-
-
-//            TODO: Fix this
-//            TODO: potentially metal is not filtered out properly, and is updating everything to parent id 7
-//            DB::table('types')->whereIn('id', $nonMetals->pluck("id") )->update([
-//                "parent_id" => $nonMetals[0]->parent_id
-//            ]);
-//            dd($nonMetals->toArray());
-            DB::table('types')->upsert($nonMetals->toArray(), ['id'], ['parent_id']);
-            DB::table('types')->upsert($metals->toArray(), ['id', 'parent_id'], ['parent_id']);
-            //throw new Error("Force rollback");
-        });
+        }
     }
 
     public function down(): void
     {
         Schema::table('types', function (Blueprint $table) {
+            $table->dropForeign('types_parent_id_foreign');
             $table->dropColumn('parent_id');
-            //$table->dropForeign('types_parent_id_foreign');
         });
     }
 };
