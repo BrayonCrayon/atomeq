e872603a7e5bbd61d01f23e4d770bede94440292
e.stroganova <e.stroganova@vehikl.com>
2025-12-17 11:46:06 -0500

    Seed the types table with parent_id column data unless the app is running in test env

--- database/migrations/2025_12_17_163819_update_types_table_with_parent_id_column.php
+++ database/migrations/2025_12_17_163819_update_types_table_with_parent_id_column.php
@@ -1,23 +1,37 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     public function up(): void
     {
-        Schema::table('types', function (Blueprint $table) {
-            $table->foreignId('parent_id')->nullable()->default(null)->references('id')->on('types');
-        });
-    }
+        if (app()->runningUnitTests()) {
+            return;
+        }
 
-    public function down(): void
-    {
-        Schema::table('types', function (Blueprint $table) {
-            $table->dropForeign('types_parent_id_foreign');
-            $table->dropColumn('parent_id');
+        DB::transaction(function () {
+            $parentsNames = ['metal', 'nonmetal', 'metalloid'];
+            $nonMetalChildrenNames = ['noble-gas', 'halogen'];
+
+            $types = DB::table('types')->get();
+            [$metal, $_, $nonmetal] = $types->whereIn('name', $parentsNames)->sortBy('name')->values()->toArray();
+            $children = $types->whereNotIn('name', $parentsNames);
+            $nonMetalKids = $types->whereIn('name', $nonMetalChildrenNames);
+            $metalKids = $children->whereNotIn('name', array_merge($parentsNames, $nonMetalChildrenNames));
+
+            $nonMetalKids = $nonMetalKids->map(function ($nonMetal) use ($nonmetal) {
+                $nonMetal->parent_id = $nonmetal->id;
+                return (array)$nonMetal;
+            });
+
+            $metalKids = $metalKids->map(function ($metalKid) use ($metal) {
+                $metalKid->parent_id = $metal->id;
+                return (array)$metalKid;
+            });
+
+            DB::table('types')->upsert($nonMetalKids->toArray(), ['id'], ['parent_id']);
+            DB::table('types')->upsert($metalKids->toArray(), ['id'], ['parent_id']);
         });
     }
 };
