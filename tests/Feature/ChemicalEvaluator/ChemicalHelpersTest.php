0e548166414692a1babac85fe03d3cb83da35bee
Brady Deroy <b.deroy@vehikl.com>
2026-06-17 16:54:14 -0400

    create migration for multiple valency column
    Co-authored-by: Vanesa<vanesa@robledo.ca>
    Co-authored-by: Josiah<josiah.williams.dev@gmail.com>

--- tests/Feature/ChemicalEvaluator/ChemicalHelpersTest.php
+++ tests/Feature/ChemicalEvaluator/ChemicalHelpersTest.php
@@ -5,12 +5,34 @@ use App\Models\Element;
 
 uses(ChemicalHelpers::class);
 
-test('can lookup valency of Nitrogen', function () {
-    Element::factory()->create([
-        'name' => 'Nitrogen',
-        'symbol' => 'N',
-        'valency' => 3
-    ]);
-    $result = $this->valencyLookup('N');
-    expect($result)->toBe(3);
+describe('valencyLookup', function () {
+    test('can lookup valency of Nitrogen', function () {
+        Element::factory()->create([
+            'name' => 'Nitrogen',
+            'symbol' => 'N',
+            'valency' => 3
+        ]);
+        $result = $this->valencyLookup('N');
+        expect($result)->toBe(3);
+    });
+});
+
+describe('calculateValency', function () {
+   test('will calculate N valency with H', function () {
+        $Scandium = Element::factory()->create([
+            'name' => 'Scandium',
+            'symbol' => 'Sc',
+            'valency' => null,
+            'atomic_number' => 21
+        ]);
+        $oxygen = Element::factory()->create([
+            'name' => 'Oxygen',
+            'symbol' => 'O',
+            'valency' => 2,
+            'atomic_number' => 8
+        ]);
+
+        $result = $this->calculateValency($element, $element);
+        expect($result)->toBe(4);
+    });
 });
