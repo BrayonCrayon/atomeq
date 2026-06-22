ded22e412ee95cfa8dca7f309b8db7fb1894589c
Brady Deroy <b.deroy@vehikl.com>
2026-06-22 16:20:18 -0400

    Complete migration to add relationship for valencies to elements
    Co-authored-by: Xiao <x.xue@vehikl.com>

--- database/migrations/2026_06_17_205214_modify_elements_table_with_mulitple_valency_column.php
+++ database/migrations/2026_06_17_205214_modify_elements_table_with_mulitple_valency_column.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Element;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -8,11 +9,26 @@ return new class extends Migration
 {
     public function up(): void
     {
-        //
+        Schema::create('element_valencies', function (Blueprint $table) {
+            $table->foreignIdFor(Element::class)->index();
+            $table->smallInteger('valency');
+        });
+
+        $existingValencyRows = DB::table('elements')
+            ->select(['id','valency'])
+            ->whereNotNull('valency')
+            ->get();
+
+        $existingValencyRows->each(function ($row) {
+            DB::table('element_valencies')->insert([
+                'element_id' => $row->id,
+                'valency' => $row->valency,
+            ]);
+        });
     }
 
     public function down(): void
     {
-        //
+        Schema::dropIfExists('element_valencies');
     }
 };
