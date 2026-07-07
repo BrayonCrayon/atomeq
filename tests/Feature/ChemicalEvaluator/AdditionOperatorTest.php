88f3b77fad022aed657527ecb291d9cf6af84e9c
Brady Deroy <b.deroy@vehikl.com>
2026-07-07 16:55:43 -0400

    wip add test for multiple valencies reactants for addition operator
    Co-authored-by: Drew <d.redford@vehikl.com>
    Co-authored-by: Kate <e.stroganova@vehikl.com>
    Co-authored-by: Brandon <b.cormier@vehikl.com>
    Co-authored-by: Rodrigo <r.wongmac@vehikl.com>

--- tests/Feature/ChemicalEvaluator/AdditionOperatorTest.php
+++ tests/Feature/ChemicalEvaluator/AdditionOperatorTest.php
@@ -29,5 +29,27 @@ test('will correctly assign atoms based on their valency', function () {
     expect($result->substances)->toHaveCount(2);
     expect($result->substances[0]->atom)->toBe(1);
     expect($result->substances[1]->atom)->toBe(1);
+});
 
+test('will correctly assign atoms based on their valency when multiple valencies is available and a specific valency is provided', function () {
+    Cache::flush();
+    $ironElement = Element::factory()->create(['name' => 'Iron', 'symbol' => 'Fe']);
+    Valency::factory()->for($ironElement)
+        ->sequence(
+            ['valency' => 2],
+            ['valency' => 3]
+        )->create();
+    $oxygenElement = Element::factory()->create(['name' => 'Oxygen', 'symbol' => 'O']);
+    Valency::factory()->for($oxygenElement)
+        ->sequence(['valency' => 2])->create();
+
+    $left = new Reactant('Fe<sub>3</sub>');
+    $right = new Reactant('O<sub>2</sub>');
+
+    $additionOperator = new AdditionOperator();
+    $result = $additionOperator->operate($left, $right);
+
+    expect($result->substances)->toHaveCount(2);
+    expect($result->substances[0]->atom)->toBe(2);
+    expect($result->substances[1]->atom)->toBe(3);
 });
