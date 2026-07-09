a8a6395120790de543e324fdc33400b978b15321
Brady Deroy <b.deroy@vehikl.com>
2026-07-09 16:59:48 -0400

    Looking into Reactivity Series
    Co-authored-by: Drew <d.redford@vehikl.com>
    Co-authored-by: Rodrigo <r.wongmac@vehikl.com>
    Co-authored-by: Vanesa <v.robledo@vehikl.com>

--- tests/Feature/ChemicalEvaluator/AdditionOperatorTest.php
+++ tests/Feature/ChemicalEvaluator/AdditionOperatorTest.php
@@ -43,8 +43,6 @@ test('will correctly assign atoms based on their valency when multiple valencies
     Valency::factory()->for($oxygenElement)
         ->sequence(['valency' => 2])->create();
 
-    // Cu + O
-    // Cu2O
     $left = new Reactant('Cu');
     $right = new Reactant('O');
 
@@ -56,3 +54,6 @@ test('will correctly assign atoms based on their valency when multiple valencies
     expect($result->substances[1]->atom)->toBe(1);
     expect($result->__toString())->toBe("Cu<sub>2</sub>O");
 });
+
+//TODO: Apply Reactivity Series to elements:
+// 3. Simplify Using a Single "Activity Series" RankIf your calculator is only dealing with basic single-displacement reactions (like high school chemistry), managing multiple potentials can become overly complex for your code.Instead of storing raw voltage values, you can create a simplified, single database attribute called reactivity_rank. You simply number the elements from 1 (most reactive, like Potassium) to 25+ (least reactive, like Gold) based on the standard Activity Series of Metals.Mg: reactivity_rank = 5Cu: reactivity_rank = 15Logic: If element_A.reactivity_rank < element_B.reactivity_rank, the reaction happens.
