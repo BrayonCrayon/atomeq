eddf18fb8a61db111c0c8691e60bcbf5aaa11e98
Brady Deroy <b.deroy@vehikl.com>
2026-07-15 15:59:55 -0400

    Add migration for polyatomic ions table
    Co-authored-by: Drew <d.redford@vehikl.com>
    Co-authored-by: Rodrigo <r.wongmac@vehikl.com>
    Co-authored-by: Vanesa <v.robledo@vehikl.com>
    Co-authored-by: Zahra <z.hussain@vehikl.com>

---
+++ database/migrations/2026_07_15_195506_create_polyatomic_ions_table.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('polyatomic_ions', function (Blueprint $table) {
+            $table->id();
+            $table->string('symbol');
+            $table->integer('charge');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('polyatomic_ions');
+    }
+};
